02a5deee05a9dd5f54f933c5a7da0505b76d560f
dungpk14789 <62883445+dungpk14789@users.noreply.github.com>
2021-08-13 19:00:03 +0700

    singleton instance

--- CreationalPattern/Singleton/SingletonPattern/SingletonPattern/Program.cs
+++ CreationalPattern/Singleton/SingletonPattern/SingletonPattern/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace SingletonPattern
 {
@@ -10,6 +11,17 @@ namespace SingletonPattern
     {
         static void Main(string[] args)
         {
+            var thread1 = new Thread(() => SingletonPattern.GetInstance().SingletonNumber());
+            var thread2 = new Thread(() => SingletonPattern.GetInstance().SingletonNumber());
+            var thread3 = new Thread(() => SingletonPattern.GetInstance().SingletonNumber());
+            var thread4 = new Thread(() => SingletonPattern.GetInstance().SingletonNumber());
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread4.Start();
+
+            Console.Read();
         }
     }
 }
---
+++ CreationalPattern/Singleton/SingletonPattern/SingletonPattern/SingletonPattern.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class SingletonPattern
+    {
+        private int index;
+        private static readonly object lockObject = new object();
+        private static volatile SingletonPattern uniqueInstance;
+
+        private SingletonPattern(int index)
+        {
+            this.index = index;
+        }
+
+        public static SingletonPattern GetInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                lock (lockObject)
+                {
+                    var random = new Random();
+
+                    uniqueInstance = new SingletonPattern(random.Next(1, 4));
+                }
+            }
+            return uniqueInstance;
+        }
+
+        public void SingletonNumber()
+        {
+            Console.WriteLine("Singleton number " + index);
+        }
+    }
+}
