1bf8d7b1feccf2f490ba54d262e36c05f14884c9
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2021-11-30 10:22:53 +0700

    add exBuilder

---
+++ CreationalPatterns/Builder/exBuilder/exBuilder/Director.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exBuilder
+{
+    public class Director
+    {
+        private IBuilder _builder;
+
+        public IBuilder Builder
+        {
+            set { _builder = value; }
+        }
+        public void buildMinimalViableProduct()
+        {
+            this._builder.BuildPartA();
+        }
+
+        public void buildFullFeaturedProduct()
+        {
+            this._builder.BuildPartA();
+            this._builder.BuildPartB();
+            this._builder.BuildPartC();
+        }
+    }
+}
---
+++ CreationalPatterns/Builder/exBuilder/exBuilder/IBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exBuilder
+{
+    public interface IBuilder
+    {
+        void BuildPartA();
+
+        void BuildPartB();
+
+        void BuildPartC();
+    }
+}
