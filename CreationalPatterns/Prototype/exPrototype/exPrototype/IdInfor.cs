6c517b8d261b57e4f813e88a7c10de93b29ed439
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2021-11-30 10:23:07 +0700

    add exPrototype

---
+++ CreationalPatterns/Prototype/exPrototype/exPrototype/IdInfor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exPrototype
+{
+    public class IdInfor
+    {
+        public int IdNumber;
+
+        public IdInfor(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+}
---
+++ CreationalPatterns/Prototype/exPrototype/exPrototype/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exPrototype
+{
+    public class Person
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfor IdInfor;
+
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person clone = (Person)this.MemberwiseClone();
+            clone.IdInfor = new IdInfor(IdInfor.IdNumber);
+            clone.Name = String.Copy(Name);
+            return clone;
+        }
+    }
+}
