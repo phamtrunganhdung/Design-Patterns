6dddc6015befe75b8940c101b1449067e115d717
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2021-12-14 08:19:49 +0700

    exCode of Dofactory

---
+++ BehavioralPatterns/Interpreter/exInterpreter/exInterpreter/Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exInterpreter
+{
+    public class Context
+    {
+        string input;
+        int output;
+        // Constructor
+        public Context(string input)
+        {
+            this.input = input;
+        }
+        public string Input
+        {
+            get { return input; }
+            set { input = value; }
+        }
+        public int Output
+        {
+            get { return output; }
+            set { output = value; }
+        }
+    }
+}
---
+++ BehavioralPatterns/Interpreter/exInterpreter/exInterpreter/NonterminalExpression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exInterpreter
+{
+    public class NonterminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("Called Nonterminal.Interpret()");
+        }
+    }
+}
