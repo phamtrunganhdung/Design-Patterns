21fecd9c4f5e7696c5ccd783ddd68c8bcfd36ee6
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2021-12-14 08:32:31 +0700

    exCode of Refactoring guru

---
+++ BehavioralPatterns/ChainOfResp/exChainOfResp/exChainOfResp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exChainOfResp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var monkey = new MonkeyHandler();
+            var squirrel = new SquirrelHandler();
+            var dog = new DogHandler();
+
+            monkey.SetNext(squirrel).SetNext(dog);
+
+            Console.WriteLine("Chain: Monkey > Squirrel > Dog\n");
+            Client.ClientCode(monkey);
+            Console.WriteLine();
+
+            Console.WriteLine("Subchain: Squirrel > Dog\n");
+            Client.ClientCode(squirrel);
+        }
+    }
+}
---
+++ BehavioralPatterns/Command/exComand/exComand/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exComand
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+}
